6d2ca051c52ff13524d370ba9be18bdd38d81bef
Asad Irfan <asadirfan446@gmail.com>
2022-08-09 04:39:58 +0500

    send email on submit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,26 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Button } from '@mui/material';
+import emailjs, { init } from "@emailjs/browser";
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 const theme = createTheme();
+
+function BasicAlerts({ type, title, message }) {
+  return (
+    <Stack sx={{ width: '100%' }} spacing={2} style={{ marginBottom: 20 }}>
+      <Alert severity={type}>
+        <AlertTitle>{title}</AlertTitle>
+        {message}
+      </Alert>
+    </Stack>
+
+  );
+}
 
 const FileUploader = (props) => {
 
@@ -32,11 +50,13 @@ const FileUploader = (props) => {
 };
 
 const Form = () => {
+  const [alert, setAlert] = React.useState({ show: false })
+  const [loading, setLoading] = React.useState(false)
 
   const [form, setForm] = React.useState({
-    first_name: '',
-    last_name: '',
-    description: '',
+    first_name: null,
+    last_name: null,
+    description: null,
     email: '',
   })
 
@@ -56,16 +76,54 @@ const Form = () => {
   }
 
   const handleInputChange = (event) => {
+    setAlert({ show: false })
     setForm({
       ...form,
       [event.target.name]: event.target.value,
     })
   }
-  const handleSubmit = () => {
-  }
+
   const handleChangeImages = (images) => {
     setImages(images)
   }
+
+  const handleSubmit = () => {
+    setLoading(true)
+
+    emailjs.send('service_jlfzgv2', 'template_6o3xbm5', form, '3RdYEgS1TaUVHBfZ6')
+      .then(function (response) {
+        setForm({
+          first_name: '',
+          last_name: '',
+          description: '',
+          email: '',
+        })
+        setLoading(false)
+        setAlert({
+          show: true,
+          type: "success",
+          title: "Success",
+          message: "Email send successfully. please check your inbox for form details"
+        })
+        setTimeout(() => {
+          setForm({
+            first_name: null,
+            last_name: null,
+            description: null,
+            email: '',
+          })            
+        }, 1000);
+      }, function (error) {
+        setLoading(false)
+        console.log('FAILED...', error);
+        setAlert({
+          show: true,
+          type: 'error',
+          title: 'Error',
+          message: "Something went wrong.",
+        })
+      });
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -76,6 +134,9 @@ const Form = () => {
           autoComplete="off"
           onSubmit={handleSubmit}
           noValidate >
+
+          {alert?.show && <BasicAlerts type={alert.type} title={alert.title} message={alert.message} />}
+
           <h2> Simple Form </h2>
           <Grid container spacing={2} style={{ marginBottom: 20 }}>
             <Grid item xs={12} sm={12} md={6}>
@@ -134,11 +195,20 @@ const Form = () => {
             label='Email Address'
             name="email"
             autoComplete="email"
-            error={!isValidEmail(form.email)}
-            helperText={!isValidEmail(form.email) ? 'Email is invalid.' : ''}
+            error={form.email && !isValidEmail(form.email)}
+            helperText={form.email && !isValidEmail(form.email) ? 'Email is invalid.' : ''}
             style={{ width: '100%', marginBottom: 20 }}
 
           />
+
+          {loading &&
+            <div className='loader_position'>
+              <span style={{ fontSize: 28 }}>
+                Uploading Form
+              </span>
+              <LinearProgress style={{ marginTop: 18, marginBottom: 30 }} />
+            </div>
+          }
 
           <div className='images'>
             <Grid container spacing={2} style={{ marginBottom: 20 }}>
@@ -151,6 +221,7 @@ const Form = () => {
             <FileUploader handleChangeImages={handleChangeImages} images={images} />
             <Button variant="contained"
               disabled={!form.first_name || !form.last_name || !form.description || !form.email}
+              onClick={() => handleSubmit()}
             >Save</Button>
           </div>
         </Box>
@@ -160,6 +231,7 @@ const Form = () => {
 }
 
 const App = () => {
+  init("3RdYEgS1TaUVHBfZ6");
 
   return (
     <div className="App">
